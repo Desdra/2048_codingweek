b32dc0c0d7ca2ae6e933ac7f151bb563a20ecb3e
2018louysa <arnaud.louys@student-cs.fr>
2018-11-13 14:48:54 +0100

    init_grid added

--- game2048/grid_2048.py
+++ game2048/grid_2048.py
@@ -1,5 +1,18 @@
+import random
+
 def create_grid():
     grid = []
     for i in range(0,4):
         grid.append([' ',' ',' ', ' '])
     return grid
+
+def init_grid(grid):
+    i2,j2 = random.randint(0,3),random.randint(0,3)
+    grid[i2][j2] = 2
+    i4,j4 = random.randint(0,3),random.randint(0,3)
+    while i2 == i4 and j2 == j4:
+        i4,j4 = random.randint(0,3),random.randint(0,3)
+    grid[i4][j4] = 4
+    return grid
+
+print(init_grid(create_grid()))
--- test_grid_2048.py
+++ test_grid_2048.py
@@ -1,6 +1,17 @@
-from game2048.grid_2048 import create_grid
+from game2048.grid_2048 import *
 from pytest import *
 
 
 def test_create_grid():
     assert create_grid() == [[' ',' ',' ', ' '],[' ',' ',' ', ' '],[' ',' ',' ', ' '],[' ',' ',' ', ' ']]
+
+def test_init_grid():
+    grid = init_grid(create_grid())
+    twoPresent,fourPresent = 0,0
+
+    for i in grid:
+        if 2 in i:
+            twoPresent = 1 - twoPresent
+        if 4 in i:
+            fourPresent = 1 - fourPresent
+    assert twoPresent and fourPresent
