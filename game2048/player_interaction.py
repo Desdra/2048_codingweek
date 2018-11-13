a892e25cd55097565ad80db3c84a16408a1f7268
2018louysa <arnaud.louys@student-cs.fr>
2018-11-13 16:45:35 +0100

    mise a jour des tests

--- game2048/player_interaction.py
+++ game2048/player_interaction.py
@@ -1,10 +1,15 @@
 from game2048.themes import THEMES
 
-def player_setup():
+def player_setup_theme():
     theme_number = input("Rentrez le numéro du thème que vous souhaitez avoir")
     if not(theme_number in THEMES.keys()):
         return None
     return theme_number
+
+def player_setup_size():
+    size_number = input("Rentrez la taille de grille que vous souhaitez avoir")
+    return size_number
+
 
 def get_player_direction():
     direction = input("Dans quelle direction voulez-vous jouez ? (d : droite, g : gauche, h : haut, b : bas)")
--- test/test_player_interaction.py
+++ test/test_player_interaction.py
@@ -1,14 +1,20 @@
 from game2048.player_interaction import *
 from pytest import *
 
-def test_player_setup(monkeypatch):
+def test_player_theme(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda x: "0")
-    assert player_setup() == "0"
+    assert player_setup_theme() == "0"
     monkeypatch.setattr('builtins.input', lambda x: "4")
-    assert player_setup() == None
+    assert player_setup_theme() == None
 
 def test_get_player_direction(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda x: "d")
     assert get_player_direction() == 0
     monkeypatch.setattr('builtins.input', lambda x: "h")
     assert get_player_direction() == 2
+
+def test_player_setup_size(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda x: 4)
+    assert player_setup_size() == 4
+    monkeypatch.setattr('builtins.input', lambda x: 2)
+    assert player_setup_size() == 2
