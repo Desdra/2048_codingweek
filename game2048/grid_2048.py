def6a970c794595b1eec7468d92624f83491e2d8
2018louysa <arnaud.louys@student-cs.fr>
2018-11-13 16:41:02 +0100

    modification de l'arborescence et correction de bug

--- game2048/grid_2048.py
+++ game2048/grid_2048.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
+from game2048.themes import THEMES
 
-THEMES = {"0": {"name": "Default", 0: "", 2: "2", 4: "4", 8: "8", 16: "16", 32: "32", 64: "64", 128: "128", 256: "256", 512: "512", 1024: "1024", 2048: "2048", 4096: "4096", 8192: "8192"}, "1": {"name": "Chemistry", 0: "", 2: "H", 4: "He", 8: "Li", 16: "Be", 32: "B", 64: "C", 128: "N", 256: "O", 512: "F", 1024: "Ne", 2048: "Na", 4096: "Mg", 8192: "Al"}, "2": {"name": "Alphabet", 0: "", 2: "A", 4: "B", 8: "C", 16: "D", 32: "E", 64: "F", 128: "G", 256: "H", 512: "I", 1024: "J", 2048: "K", 4096: "L", 8192: "M"}}
 theme = THEMES['0']
 
 def create_grid():
@@ -20,8 +20,8 @@ def init_grid():
     grid[i4][j4] = 4
     return grid
 
-def display_grid(grid,theme):
-    size = theme_size(theme)
+def display_grid(grid,theme,theme_size_calc, player_size):
+    size = max(theme_size_calc, player_size)
     string = ""
     for i in range(4):
         string += " "
