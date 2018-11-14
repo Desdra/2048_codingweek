404e1561135e3aa9c7189739f23bc2bf9837f267
2018louysa <arnaud.louys@student-cs.fr>
2018-11-14 16:08:20 +0100

    prevent user mis-input

--- game2048/player_interaction_2048.py
+++ game2048/player_interaction_2048.py
@@ -4,12 +4,14 @@ def player_setup_theme():
     """ask and return the theme wanted by the player"""
     theme_number = input("Rentrez le numéro du thème que vous souhaitez avoir")
     if not(theme_number in THEMES.keys()):
-        return None
+        theme_number = input("Numéro invalide, réessayer s'il vous plaît")
     return theme_number
 
 def player_setup_size():
     """ask and return the size of the grid wanted by the player"""
     size_number = input("Rentrez la taille de grille que vous souhaitez avoir")
+    while size_number.isdigit() == False:
+        size_number = input("Entrée invalide, réessayer s'il vous plaît")
     return int(size_number)
 
 
